4466289ced7a9a1f784ae94423bfaf52070649c3
Subneed17 <Vincentcabon0107gmail.com>
2021-06-07 17:15:59 +0200

    Quête 11

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,9 +1,10 @@
 <?php
-// src/Controller/ProgramController.php
+
 namespace App\Controller;
 
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Entity\Episode;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,45 +32,60 @@ class ProgramController extends AbstractController
         );
     }
     /**
-     * Getting a program by id
-     *
      * @Route("/show/{id<^[0-9]+$>}", name="show")
+     * @param Program $program
      * @return Response
      */
-    public function show(int $id): Response
+    public function show(Program $program): Response
     {
-        $program = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findOneBy(['id' => $id]);
-
-        if (!$program) {
-            throw $this->createNotFoundException(
-                'No program with id : ' . $id . ' found in program\'s table.'
-            );
-        }
         return $this->render('program/show.html.twig', [
             'program' => $program,
         ]);
     }
-       /**
-     * Getting a program by id
-     *
-     * @Route("/{programId<^[0-9]+$>}/season/{seasonId<^[0-9]+$>}/", name="season_show")
+
+    
+    /**
+     * @route("/{program<^[0-9]+$>}/seasons/{season<^[0-9]+$>}}", methods={"GET"}, name="season_show")
+     * @param Program $program
+     * @param Season $season
      * @return Response
      */
-    public function showSeason(int $programId, int $seasonId) : Response 
+    public function showSeason(Program $program, Season $season): response
     {
-        $program = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findOneBy(['id' => $programId]);
-
-        $season= $this->getDoctrine()
-            ->getRepository(Season::class)
-            ->findOneBy(['id' => $seasonId]);
-
         return $this->render('program/show_season.html.twig', [
             'program' => $program,
             'season' => $season
+        ]);
+    }
+
+    //    /**
+    //  * @Route("/{program<^[0-9]+$>}/seasons/{season<^[0-9]+$>}}, name="season_show")
+    //  * @param Program $program
+    //  * @param Season $season
+    //  * @return Response
+    //  */
+    // public function showSeason(Program $program, Season $season) : Response 
+    // {
+    //     return $this->render('program/show_season.html.twig', [
+    //         'program' => $program,
+    //         'season' => $season
+    //     ]);
+    // }
+
+
+     /**
+     * @route("/{program}/seasons/{season}/episodes/{episode}", name="episode_show")
+     * @param Program $program
+     * @param Season $season
+     * @param Episode $episode
+     * @return Response
+     */
+    public function showEpisode(Program $program, Season $season, Episode $episode) : Response 
+    {
+        return $this->render('program/episode_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
+            'episode' => $episode
         ]);
 
     }
