39de4805c327681e3c101e7f952ccf52f8a55556
Subneed17 <Vincentcabon0107gmail.com>
2021-06-08 11:36:15 +0200

    Quête 12 finished

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Program;
 use App\Entity\Season;
 use App\Entity\Episode;
+use App\Form\ProgramType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -31,6 +33,38 @@ class ProgramController extends AbstractController
             ['programs' => $programs]
         );
     }
+
+
+    /**
+     * The controller for the category add form
+     * Display the form or deal with it
+     *
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request): Response
+    {
+       // Create a new Program Object
+       $program = new Program();
+       // Create the associated Form
+       $form = $this->createForm(ProgramType::class, $program);
+       // Get data from HTTP request
+       $form->handleRequest($request);
+       // Was the form submitted ?
+       if ($form->isSubmitted()) {
+           // Deal with the submitted data
+           // Get the Entity Manager
+           $entityManager = $this->getDoctrine()->getManager();
+           // Persist Category Object
+           $entityManager->persist($program);
+           // Flush the persisted object
+           $entityManager->flush();
+           // Finally redirect to categories list
+           return $this->redirectToRoute('program_index');
+        }
+        // Render the form
+        return $this->render('program/new.html.twig', ["form" => $form->createView()]);
+    }
+
     /**
      * @Route("/show/{id<^[0-9]+$>}", name="show")
      * @param Program $program
@@ -57,20 +91,6 @@ class ProgramController extends AbstractController
             'season' => $season
         ]);
     }
-
-    //    /**
-    //  * @Route("/{program<^[0-9]+$>}/seasons/{season<^[0-9]+$>}}, name="season_show")
-    //  * @param Program $program
-    //  * @param Season $season
-    //  * @return Response
-    //  */
-    // public function showSeason(Program $program, Season $season) : Response 
-    // {
-    //     return $this->render('program/show_season.html.twig', [
-    //         'program' => $program,
-    //         'season' => $season
-    //     ]);
-    // }
 
 
      /**
